fd0a84544e9baafaab44f56bf2396b859b929840
alirezaradzadegan1996 <_>
2026-07-15 18:07:52 +0330

    feat(cortex): add skill manager layer

--- core/cortex/cortex.py
+++ core/cortex/cortex.py
@@ -5,6 +5,7 @@ Sepehr2 Cortex
 
 from .registry import registry
 from .engine import CortexEngine
+from .skill_manager import SkillManager
 
 
 class Cortex:
@@ -12,29 +13,61 @@ class Cortex:
     def __init__(self):
         self.registry = registry
         self.engine = CortexEngine(self.registry)
+        self.skill_manager = SkillManager()
+
 
     def register(self, name, service):
         self.registry.register(name, service)
 
+
     def unregister(self, name):
         self.registry.unregister(name)
+
 
     def has(self, name):
         return self.registry.has(name)
 
+
     def get(self, name):
         return self.registry.get(name)
 
+
     def execute(self, name, *args, **kwargs):
-        return self.engine.execute(name, *args, **kwargs)
+        return self.engine.execute(
+            name,
+            *args,
+            **kwargs
+        )
+
 
     def services(self):
         return self.registry.list()
+
+
+    # -------------------------
+    # Skill Management
+    # -------------------------
+
+    def set_skills(self, skills):
+        self.skill_manager = SkillManager(skills)
+
+
+    def find_skill(self, text, intent=None):
+        return self.skill_manager.find(
+            text,
+            intent
+        )
+
+
+    def skills(self):
+        return self.skill_manager.list()
+
 
     def status(self):
         return {
             "services": self.registry.list(),
             "engine": self.engine.report(),
+            "skills": self.skill_manager.list()
         }
 
 
