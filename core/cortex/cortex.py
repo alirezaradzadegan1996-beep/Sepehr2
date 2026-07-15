59342e2fc3319874af4c072c2f1d0be0f5da8d1c
alirezaradzadegan1996 <_>
2026-07-15 16:26:18 +0330

    feat(cortex): integrate registry and engine

--- core/cortex/cortex.py
+++ core/cortex/cortex.py
@@ -1,18 +1,41 @@
+"""
+Sepehr2 Cortex
+مرکز مدیریت تمام سرویس‌های سیستم
+"""
+
+from .registry import registry
+from .engine import CortexEngine
+
+
 class Cortex:
 
     def __init__(self):
+        self.registry = registry
+        self.engine = CortexEngine(self.registry)
 
-        self.modules = {}
+    def register(self, name, service):
+        self.registry.register(name, service)
 
-    def register(self, name, module):
+    def unregister(self, name):
+        self.registry.unregister(name)
 
-        self.modules[name] = module
+    def has(self, name):
+        return self.registry.has(name)
 
     def get(self, name):
+        return self.registry.get(name)
 
-        return self.modules.get(name)
+    def execute(self, name, *args, **kwargs):
+        return self.engine.execute(name, *args, **kwargs)
+
+    def services(self):
+        return self.registry.list()
 
     def status(self):
+        return {
+            "services": self.registry.list(),
+            "engine": self.engine.report(),
+        }
 
-        return list(self.modules.keys())
 
+cortex = Cortex()
