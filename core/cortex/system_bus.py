fcc081c64c93d0dd2cdcace183310d21e95f7393
alirezaradzadegan1996 <_>
2026-07-15 16:41:01 +0330

    feat(cortex): upgrade system bus to event bus

--- core/cortex/system_bus.py
+++ core/cortex/system_bus.py
@@ -1,13 +1,25 @@
+"""
+Sepehr2 System Bus
+Shared data + Event Bus
+"""
+
+
 class SystemBus:
 
     def __init__(self):
         self.data = {}
+        self.events = {}
+        self.history = []
+
+    # -------------------------
+    # Shared Data
+    # -------------------------
 
     def set(self, key, value):
         self.data[key] = value
 
-    def get(self, key):
-        return self.data.get(key)
+    def get(self, key, default=None):
+        return self.data.get(key, default)
 
     def remove(self, key):
         if key in self.data:
@@ -16,5 +28,39 @@ class SystemBus:
     def clear(self):
         self.data.clear()
 
+    # -------------------------
+    # Events
+    # -------------------------
+
+    def subscribe(self, event, callback):
+        self.events.setdefault(event, []).append(callback)
+
+    def unsubscribe(self, event, callback):
+        if event not in self.events:
+            return
+
+        if callback in self.events[event]:
+            self.events[event].remove(callback)
+
+    def publish(self, event, *args, **kwargs):
+        self.history.append(event)
+
+        for callback in self.events.get(event, []):
+            try:
+                callback(*args, **kwargs)
+            except Exception as e:
+                print(f"[SystemBus] {event} -> {e}")
+
+    # -------------------------
+    # Status
+    # -------------------------
+
     def status(self):
-        return self.data
+        return {
+            "data": list(self.data.keys()),
+            "events": list(self.events.keys()),
+            "history": len(self.history),
+        }
+
+
+bus = SystemBus()
